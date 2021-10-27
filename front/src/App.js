79a547e6e06b61ce21941e1bbb9df6078f8b0f80
Luis Saturno <saturno.luis@gmail.com>
2021-10-27 16:40:30 -0300

    Add styles for App markdown

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -6,12 +6,12 @@ function App() {
       <header className="App__header">
         <h1>My Tasks</h1>
       </header>
-      <main className="App__header">
+      <main className="App__body">
         <ul>
           {dummyData().map(({ title, done, uuid }, i) => (
             <li key={uuid}>
               <article>
-                <h1>Task #{i}</h1>
+                <h1>Task #{i + 1}</h1>
                 <p>{title}</p>
               </article>
             </li>
@@ -35,32 +35,6 @@ export default App;
 
 function dummyData() {
   return (
-    [
-      {
-        "title": "possimus dolorem et",
-        "done": false,
-        "uuid": "0eb516a0-d3d7-45ab-9e3b-7fd01d6cefee"
-      },
-      {
-        "title": "veritatis distinctio modi",
-        "done": false,
-        "uuid": "cc3601df-9dbb-4d68-87f6-42e62259379d"
-      },
-      {
-        "title": "sequi maxime corporis",
-        "done": false,
-        "uuid": "205aa0b5-dd1f-480d-b3b6-851dfdd66a81"
-      },
-      {
-        "title": "nesciunt error magnam",
-        "done": false,
-        "uuid": "2c3e6f2f-7cf2-498d-942e-afa0c7826e9d"
-      },
-      {
-        "title": "unde minima quia",
-        "done": false,
-        "uuid": "4f33faf9-3890-49ff-a67e-df33ba3a221a"
-      }
-    ]
+    [{"title":"et quas est","done":false,"uuid":"b4506c05-ec45-45b4-b0a7-3cf380f30ef4"},{"title":"voluptatibus sed sit","done":false,"uuid":"1b6c65ae-c86b-4441-a289-03395e335585"},{"title":"accusamus similique dolorem","done":false,"uuid":"01500b28-614f-47d3-af9d-cc8869d7017a"},{"title":"veniam deserunt eligendi","done":false,"uuid":"66385592-7dd8-4800-ad8a-f11c7deb0a90"},{"title":"ut quaerat quidem","done":false,"uuid":"f9a1950d-69b2-4698-8c76-2006b5aed285"}]
   );
 }
